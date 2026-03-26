7bd61b27dbf3dceb1934767ad36e77f4a4268262
Prosper Love <your_email@example.com>
2026-03-26 15:20:18 -0700

    Updated site design and fixes

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import site from "../Image/site.png"
 import site2 from "../Image/site2.png"
 import site3 from "../Image/site3.png"
+import site4 from "../Image/site4.png"
 import devcode from "../Image/devcode.png"
 import sg from "../Image/sg.png"
 
@@ -15,6 +16,50 @@ const Projects = () => {
   <div className="container">
     <div className="row g-4">
       
+        {/* Project 2 */}
+      <div className="col-12 col-md-6 col-lg-4">
+        <div className="card bg-dark text-light  shadow-lg border-0">
+          <img src={site4} className="card-img-top" alt="Todo App" />
+          <div className="card-body">
+            <h5 className="card-title" style={{ color: "aqua" }}>Web3 Application</h5>
+            <p className="card-text" style={{ color: "#cacad6" }}>
+            Web3/Crypto trading web application (flick UI)
+            </p>
+            <p style={{ fontSize: "0.9rem", color: "#8fd6ff" }}>
+              <strong>Languages Used:</strong>React, Tailwind, JavaScript,Motion
+            </p>
+            <a href="https://cryptonaviapp.vercel.app/" target="_blank" rel="noopener noreferrer" className="btn btn-outline-info me-2">
+              View Project
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light">
+              Source Code
+            </a>
+          </div>
+        </div>
+      </div>
+
+       {/* Project 3 */}
+          <div className="col-12 col-md-6 col-lg-4">
+        <div className="card bg-dark text-light  shadow-lg border-0">
+          <img src={devcode} className="card-img-top" alt="Todo App" />
+          <div className="card-body">
+            <h5 className="card-title" style={{ color: "aqua" }}>Buisness Websiness</h5>
+            <p className="card-text" style={{ color: "#cacad6" }}>
+           A tech startup Porfolio website for Devcode Hub
+            </p>
+            <p style={{ fontSize: "0.9rem", color: "#8fd6ff" }}>
+              <strong>Languages Used:</strong> React,Tailwindcss, Javascript
+            </p>
+            <a href="https://devcode-hub.vercel.app/" target="_blank" rel="noopener noreferrer" className="btn btn-outline-info me-2">
+              View Project
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light">
+              Source Code
+            </a>
+          </div>
+        </div>
+      </div>
+         
       {/* Project 1 */}
       <div className="col-12 col-md-6 col-lg-4">
         <div className="card bg-dark text-light  shadow-lg border-0">
@@ -53,28 +98,6 @@ const Projects = () => {
               View Project
             </a>
             <a href="https://github.com/prosperplove/Shopping-store" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light">
-              Source Code
-            </a>
-          </div>
-        </div>
-      </div>
-        
-          {/* Project 3 */}
-          <div className="col-12 col-md-6 col-lg-4">
-        <div className="card bg-dark text-light  shadow-lg border-0">
-          <img src={devcode} className="card-img-top" alt="Todo App" />
-          <div className="card-body">
-            <h5 className="card-title" style={{ color: "aqua" }}>Buisness Websiness</h5>
-            <p className="card-text" style={{ color: "#cacad6" }}>
-           A tech startup Porfolio website for Devcode Hub, Showcasing the company's portfolio
-            </p>
-            <p style={{ fontSize: "0.9rem", color: "#8fd6ff" }}>
-              <strong>Languages Used:</strong> React,Tailwindcss, Javascript
-            </p>
-            <a href="https://devcode-hub.vercel.app/" target="_blank" rel="noopener noreferrer" className="btn btn-outline-info me-2">
-              View Project
-            </a>
-            <a href="#" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light">
               Source Code
             </a>
           </div>
